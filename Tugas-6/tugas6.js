62ed1d4ed7990ca2699d43b6fc046d0abf081b66
Chandra Muhamad Apriana <50396186+chandraapriana@users.noreply.github.com>
2020-08-31 16:27:38 +0300

    Menambahkan Tugas 6

---
+++ Tugas-6/tugas6.js
@@ -0,0 +1,61 @@
+// javascript ES6
+
+//soal-1
+console.log("--SOAL 1--");
+let r = 1;
+const luasLingkaran = (r) => 3.14 * r * r;
+const kelilingLingkaran = (r) => 2 * 3.14 * r;
+console.log(`Luas Lingkaran dari jari-jari ${r} adalah ${luasLingkaran(r)}`);
+console.log(
+  `Keliling Lingkaran dari jari-jari ${r} adalah ${kelilingLingkaran(r)}`
+);
+
+//soal-2
+console.log("--SOAL 2--");
+let kalimat = "";
+// const addWord = (first, second, third, four, fifth) =>
+//   (kalimat += `${first} ${second} ${third} ${four} ${fifth}`);
+
+const addWord = (word) => (kalimat += `${word} `);
+addWord("Saya");
+addWord("adalah");
+addWord("seorang");
+addWord("frontend");
+addWord("developer");
+
+console.log(kalimat);
+
+//soal-3
+console.log("--SOAL 3--");
+let FirstName = "Chandra";
+let LastName = "Muhamad Apriana";
+
+const literal = {
+  FirstName,
+  LastName,
+  fullName() {
+    console.log(`${this.FirstName} ${this.LastName}`);
+  },
+};
+
+console.log(literal.fullName());
+
+//soal-4
+console.log("--SOAL 4--");
+
+let [firstName, lastName, destination, occupation] = [
+  "Harry",
+  "Potter Holt",
+  "Hogwarts React Conf",
+  "Deve-wizard Avocado",
+  "Vimulus Renderus!!!",
+];
+
+console.log(firstName, lastName, destination, occupation);
+
+//soal-5
+console.log("--SOAL 5--");
+const west = ["Will", "Chris", "Sam", "Holly"];
+const east = ["Gill", "Brian", "Noel", "Maggie"];
+let combined = [...west, ...east];
+console.log(combined);
